53be41495ab5c87c3a752cb95da478142bdd959b
kbhuyan7609-glitch <k.bhuyan7609@gmail.com>
2026-08-20 17:14:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -100,5 +100,5 @@ app.post("/api/verify-payment",(req,res)=>{
  res.json({ok:true});
 });
 
-app.get("*",(req,res)=>res.sendFile(path.join(__dirname,"public","index.html")));
+app.get("/{*splat}", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
 app.listen(process.env.PORT||3000,()=>console.log("Shiv Supplements online store running"));
